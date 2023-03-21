0bdf3ad24614a8109d67110abfd6c235fe78e702
inamnurul <aam.inam7310@gmail.com>
2023-03-21 10:35:35 +0700

    add: logic game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Example file showing a circle moving on screen
 import pygame
 import numpy as np
+import random
 
 # pygame setup
 pygame.init()
@@ -18,6 +19,8 @@ c=b-sisi
 d=a+sisi
 square=[[a,b],[d,b],[a,c],[d,c]]
 
+randomdot=[screen.get_width()/2+50,screen.get_height()/2+50]
+
 def rotate_2d_center(vectors, centers, theta):
     vector=np.array(vectors)
     center=np.array(centers)
@@ -33,15 +36,42 @@ def rotate_2d_center(vectors, centers, theta):
     
     return vector_rotated
 
+font = pygame.font.SysFont("Arial", 30)
+
+# variable setup
+number = 0
+def scale_point(point, center, scale_factor):
+    x = center[0] + scale_factor * (point[0] - center[0])
+    y = center[1] + scale_factor * (point[1] - center[1])
+    return [x, y]
+
+def is_dot_inside_square(dot, square):
+    min_x = min(square[0][0], square[1][0], square[2][0], square[3][0])
+    max_x = max(square[0][0], square[1][0], square[2][0], square[3][0])
+    min_y = min(square[0][1], square[1][1], square[2][1], square[3][1])
+    max_y = max(square[0][1], square[1][1], square[2][1], square[3][1])
+
+    if dot[0] >= min_x and dot[0] <= max_x and dot[1] >= min_y and dot[1] <= max_y:
+        return True
+    else:
+        return False
+
 
 while running:
-    
     
     for i in range(len(square)):
         result = rotate_2d_center(square[i], centersquare, np.pi/60)
         square[i][0] = result[0]
         square[i][1] = result[1]
     
+    centerscale=[(square[0][0]+square[2][0])/2,(square[0][1]+square[2][1])/2]
+    
+    if (is_dot_inside_square(randomdot,square)==True):
+        randomdot=[random.randint(30,screen.get_width()/2-50),random.randint(30,screen.get_height()/2-50)]
+        for i in range(len(square)):
+            result = scale_point(square[i], centerscale, 9/10)
+            square[i][0] = result[0]
+            square[i][1] = result[1]
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -51,12 +81,18 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
+    number_text = font.render(str(number), True, (255, 255, 255))
+    number_rect = number_text.get_rect()
+    number_rect.bottomright = (screen.get_width() - 10, screen.get_height() - 10)
+    screen.blit(number_text, number_rect)
+    
     pygame.draw.line(screen,"white",square[0],square[1])
     pygame.draw.line(screen,"white",square[1],square[3])
     pygame.draw.line(screen,"white",square[2],square[3])
     pygame.draw.line(screen,"white",square[2],square[0]) 
     
-    pygame.draw.circle(screen,"white",centersquare,3)   
+    pygame.draw.circle(screen,"white",centersquare,3) 
+    pygame.draw.circle(screen,"red",randomdot,3)  
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
